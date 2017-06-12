26f32317e2c0507a580675265ba0724b69a6c450
Student01 <Student01@Student06s-iMac.local>
2017-06-12 14:23:26 +0700

    Variable

---
+++ Variable and Constant.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//This Comment หรือ นี่คือ Comment
+// นี่คือการประกาศตัวแปรแบบ ไม่สามารถเปลี่ยนค่าได้
+let myConstant = 123   // Implicit Constant 123
+//myConstant = 456  cannont changt let
+
+// นี่คือการประกาศตัวแปรแบบ สามารถเปลี่ยนค่าได้
+var intNumber = 123 // Implicit Variable inNumber = 123
+intNumber = 456
+
+// Explicit นี่คือการประกาศตัวแปรแบบกำหนดค่า Datatype
+let myConstant1: Int = 123
